94713445ae0a75d466cfcce54260bf1f6385af28
ridham35 <59654407+ridham35@users.noreply.github.com>
2020-06-26 11:17:55 +0530

    Third Answer

--- src/test1/C.java
+++ src/test1/C.java
@@ -25,21 +25,19 @@ public class C {
 	}
 
 	public static void mergesort() {
-		int[] arr = {15, 48, 2, 35, 18, 27, 35, 94, 43, 9};
-		sort(arr, 0, arr.length - 1);
-		System.out.print("Array sorted : " + Arrays.toString(arr) + " \n");
+		int[] array = {15, 48, 2, 35, 18, 27, 35, 94, 43, 9};
+		sort(array, 0, array.length - 1);
+		System.out.print("Array sorted : " + Arrays.toString(array) + " \n");
 	}
 
-	public static void sort(int[] arr, int left, int right) {
+	public static void sort(int[] array, int left, int right) {
 		if (left < right) {
 			int middle = (left + right) / 2;
 
-			// Sort first and second halves
-			sort(arr, left, middle);
-			sort(arr, middle + 1, right);
+			sort(array, left, middle);
+			sort(array, middle + 1, right);
 
-			// Merge the sorted halves
-			merge(arr, left, middle, right);
+			merge(array, left, middle, right);
 		}
 	}
 
@@ -47,17 +45,13 @@ public class C {
 		int n1 = middle - left + 1;
 		int n2 = right - middle;
 
-		/* Create temp arrays */
 		int[] L = new int[n1];
 		int[] R = new int[n2];
 
-		/*Copy data to temp arrays*/
+		// Copy data to temp arrays
 		if (n1 >= 0) System.arraycopy(arr, left, L, 0, n1);
 		for (int j = 0; j < n2; ++j)
 			R[j] = arr[middle + 1 + j];
-
-
-		/* Merge the temp arrays */
 
 		// Initial indexes of first and second subarrays
 		int i = 0, j = 0;
@@ -75,14 +69,14 @@ public class C {
 			k++;
 		}
 
-		/* Copy remaining elements of L[] if any */
+		// Copy elements of L[]
 		while (i < n1) {
 			arr[k] = L[i];
 			i++;
 			k++;
 		}
 
-		/* Copy remaining elements of R[] if any */
+		// Copy elements of R[]
 		while (j < n2) {
 			arr[k] = R[j];
 			j++;
